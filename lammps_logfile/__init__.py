5830d065cdb69467eb156c2fb35bfb9dd5b61901
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2020-02-04 12:43:18 +0100

    importing utils and added running average option in command line tool

--- lammps_logfile/__init__.py
+++ lammps_logfile/__init__.py
@@ -1,2 +1,3 @@
 from .File import File
+from .utils import running_mean, get_color_value, get_matlab_color
 from .cmd_interface import run
--- lammps_logfile/cmd_interface.py
+++ lammps_logfile/cmd_interface.py
@@ -1,4 +1,5 @@
 from lammps_logfile import File
+from lammps_logfile import running_mean
 import argparse
 import matplotlib.pyplot as plt
 
@@ -7,11 +8,11 @@ def get_args():
     parser.add_argument("input_file", type=str)
     parser.add_argument("-x", type=str, default="Time")
     parser.add_argument("-y", type=str, nargs="+")
+    parser.add_argument("-a", "--running_average", type=int, default=1)
     args = parser.parse_args()
     return args
 
 def run():
-    print("Hello from plotter")
     args = get_args()
     log = File(args.input_file)
     x = log.get(args.x)
@@ -19,6 +20,11 @@ def run():
     for y in args.y:
         data = log.get(y)
         print(data)
+
+        if args.running_average >= 2:
+            x = running_mean(x, args.running_average)
+            data = running_mean(data, args.running_average)
+
         plt.plot(x, data, label=y)
     plt.legend()
     plt.show()
