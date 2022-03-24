af6b03d2e644318c401295c1366d9bc1093b3f47
evenmn <evenmn@mn.uio.no>
2022-03-24 11:08:50 +0100

    Added argument '-l/--live' that allows for live plotting of log file

--- lammps_logfile/cmd_interface.py
+++ lammps_logfile/cmd_interface.py
@@ -2,6 +2,7 @@ from lammps_logfile import File
 from lammps_logfile import running_mean
 import argparse
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Plot contents from lammps log files")
@@ -11,6 +12,7 @@ def get_parser():
     parser.add_argument("-a", "--running_average", type=int, default=1, help="Optionally average over this many log entries with a running average. Some thermo properties fluctuate wildly, and often we are interested in the running average of properties like temperature and pressure.")
     parser.add_argument("-s", "--subplots", default=False, action='store_true', help="Show subplot panels for different graphs.")
     parser.add_argument("-p", "--progress", default=False, action='store_true', help="Show progress of simulation. This requires that 'Step' is written to log file.")
+    parser.add_argument("-l", "--live", default=False, action='store_true', help="Show live visualisation of graphs. This is useful when monitoring thermo outputs during simulation.")
     return parser
 
 
@@ -25,40 +27,41 @@ def create_progress_string(log):
     return f"{rel_step}/{num_steps}"
 
 
-
 def run():
     args = get_parser().parse_args()
-    log = File(args.input_file)
-    x = log.get(args.x)
-    print(x)
     if args.subplots:
         fig, ax = plt.subplots(len(args.y), sharex=True, squeeze=False)
-        for i, y in enumerate(args.y):
-            data = log.get(y)
-            print(data)
-
-            if args.running_average >= 2:
-                x = running_mean(x, args.running_average)
-                data = running_mean(data, args.running_average)
-
-            ax[i, 0].plot(x, data)
-            ax[i, 0].set_ylabel(y)
-        ax[-1, 0].set_xlabel(args.x)
     else:
         fig = plt.figure()
-        for y in args.y:
+
+    def get_data(i=0):
+        log = File(args.input_file)
+        x = log.get(args.x)
+        #print(x)
+        for i, y in enumerate(args.y):
             data = log.get(y)
-            print(data)
+            #print(data)
 
             if args.running_average >= 2:
                 x = running_mean(x, args.running_average)
                 data = running_mean(data, args.running_average)
 
-            plt.plot(x, data, label=y)
-        plt.legend()
-    if args.progress:
-        fig.suptitle(create_progress_string(log))
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+            if args.subplots:
+                ax[i, 0].cla()
+                ax[i, 0].plot(x, data)
+                ax[-1, 0].set_xlabel(args.x)
+                ax[i, 0].set_ylabel(y)
+            else:
+                plt.cla()
+                plt.plot(x, data, label=y)
+                plt.legend()
+        if args.progress:
+            fig.suptitle(create_progress_string(log))
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    
+    if args.live:
+        ani = FuncAnimation(fig, get_data, interval=1000)
+    else:
+        get_data()
     plt.show()
 
-    
